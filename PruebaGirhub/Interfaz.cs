d41c30022619666bd0ef6e0808213e25313f16cd
FranciscoA <francisfdez591@hotmail.com>
2025-02-19 08:46:00 +0100

    Estructura Organizativa del Proyecto: Clases Instanciables y Estáticas

---
+++ PruebaGirhub/Interfaz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGirhub
+{
+    /// <summary>
+    /// Clase Estática que proporiciona los métodos que posibilitan la interaccion con el usuario
+    /// </summary>
+    public static class Interfaz
+    {
+    }
+}
