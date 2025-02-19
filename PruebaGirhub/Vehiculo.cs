99b08efa0d064c59a507ae61d5c4e80baa7d3d23
FranciscoA <francisfdez591@hotmail.com>
2025-02-19 09:07:45 +0100

    Establecimiento de la estructura del modelo de datos

--- PruebaGirhub/Vehiculo.cs
+++ PruebaGirhub/Vehiculo.cs
@@ -11,5 +11,30 @@ namespace PruebaGirhub
     /// </summary>
     public class Vehiculo
     {
+
+        #region CONSTANTES - pARÁMETOS DE CONFIGGURACIÓN DE LA CLASE
+        #endregion
+
+        #region MIEMBROS | CAMPOS | ATRIBUTOS PRIVADOS
+        #endregion
+
+        #region CONSTRUCTORES
+        #endregion
+
+        // Mecanismos de acceso a la información que mantiene la clase (datos)
+        //Proporciona dos descriptores de acceso (get y e set)
+        //Su misión es la de proteger los datos que entran a la clase (set)
+        // o se transfieren fuera de la clase (get)
+
+        #region PROPIEDADES
+        #endregion
+
+        #region MÉTODOS PRIVADOS - INTERNO DE LA CLASE
+        #endregion
+
+        #region MÉTODOS PUBLICOS - ACCESIBLES DESDE FUERA DE LA CALSE
+        #endregion
+
+
     }
 }
